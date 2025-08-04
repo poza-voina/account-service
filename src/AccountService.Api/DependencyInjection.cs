88361f6469980010ec4c8bb9f7dc0231b9338113
Danil Sergeev <poza.voina@gmail.com>
2025-08-04 14:34:27 +0300

    docker compose

--- src/AccountService.Api/DependencyInjection.cs
+++ src/AccountService.Api/DependencyInjection.cs
@@ -14,8 +14,11 @@ using MediatR;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
 using System.Reflection;
+using System.Runtime.InteropServices;
 using System.Text.Json.Serialization;
 
 namespace AccountService.Api;
