588300200ee3197483061fffc16610c0e0b0a25e
Danil Sergeev <poza.voina@gmail.com>
2025-08-04 20:40:43 +0300

    fix todo

--- src/AccountService.Api/ObjectStorage/AuthenticationOptions.cs
+++ src/AccountService.Api/ObjectStorage/AuthenticationOptions.cs
@@ -1,6 +1,5 @@
 ﻿namespace AccountService.Api.ObjectStorage;
 
-// TODO: отрефачить
 public class AuthenticationOptions
 {
     /// <summary>
@@ -8,14 +7,15 @@ public class AuthenticationOptions
     /// {1} - realmName
     /// </summary>
     private const string PatternRealmSegment = "{0}/realms/{1}";
+    private const string ProtocolSegment = "/protocol/openid-connect";
     public required string Realm { get; set; }
     public required string BaseUrl { get; set; }
     public string? KeycloakDockerUrl { get; set; }
     public required bool RequireHttpsMetadata { get; set; }
     public required string Audience { get; set; }
-    public string Authority => string.Format(PatternRealmSegment, KeycloakDockerUrl ?? BaseUrl, Realm);
-    public string AuthorizationUrl => string.Format(PatternRealmSegment, BaseUrl, Realm) + "/protocol/openid-connect/auth";
-    public string TokenUrl => $"{string.Format(PatternRealmSegment, BaseUrl, Realm)}/protocol/openid-connect/token";
     public required string ClientId { get; set; }
     public required string ClientSecret { get; set; }
+    public string Authority => string.Format(PatternRealmSegment, KeycloakDockerUrl ?? BaseUrl, Realm);
+    public string AuthorizationUrl => string.Format(PatternRealmSegment, BaseUrl, Realm) + ProtocolSegment + "/auth";
+    public string TokenUrl => string.Format(PatternRealmSegment, BaseUrl, Realm) + ProtocolSegment + "/token";
 }
