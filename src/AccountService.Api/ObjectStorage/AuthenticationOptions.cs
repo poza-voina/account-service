0a8a4137fc9aa77b3906f7bc23d976c9a9ca6bca
Danil Sergeev <poza.voina@gmail.com>
2025-08-04 14:50:39 +0300

    fix

--- src/AccountService.Api/ObjectStorage/AuthenticationOptions.cs
+++ src/AccountService.Api/ObjectStorage/AuthenticationOptions.cs
@@ -1,14 +1,21 @@
 ﻿namespace AccountService.Api.ObjectStorage;
 
+// TODO: отрефачить
 public class AuthenticationOptions
 {
+    /// <summary>
+    /// {0} - baseUrl or KeycloakDockerUrl
+    /// {1} - realmName
+    /// </summary>
+    private const string PatternRealmSegment = "{0}/realms/{1}";
     public required string Realm { get; set; }
     public required string BaseUrl { get; set; }
+    public string? KeycloakDockerUrl { get; set; }
     public required bool RequireHttpsMetadata { get; set; }
     public required string Audience { get; set; }
-    public string Authority => $"{BaseUrl}/realms/{Realm}";
-    public string AuthorizationUrl => $"{Authority}/protocol/openid-connect/auth";
-    public string TokenUrl => $"{Authority}/protocol/openid-connect/token";
+    public string Authority => string.Format(PatternRealmSegment, KeycloakDockerUrl is { } ? KeycloakDockerUrl : BaseUrl, Realm);
+    public string AuthorizationUrl => string.Format(PatternRealmSegment, BaseUrl, Realm) + "/protocol/openid-connect/auth";
+    public string TokenUrl => $"{string.Format(PatternRealmSegment, BaseUrl, Realm)}/protocol/openid-connect/token";
     public required string ClientId { get; set; }
     public required string ClientSecret { get; set; }
 }
