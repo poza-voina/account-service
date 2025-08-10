a8d81fab9aa716588fbbc07fdd106023c58999a7
Danil Sergeev <poza.voina@gmail.com>
2025-08-10 14:26:33 +0300

    add dbtransactions

--- src/AccountService.Api/Features/Transactions/ExecuteTransaction/ExecuteTransactionCommandHandler.cs
+++ src/AccountService.Api/Features/Transactions/ExecuteTransaction/ExecuteTransactionCommandHandler.cs
@@ -1,18 +1,32 @@
+using AccountService.Abstractions.Exceptions;
 using AccountService.Api.Features.Transactions.ApplyTransaction;
 using AccountService.Api.Features.Transactions.RegisterTransaction;
+using AccountService.Api.ObjectStorage.Interfaces;
 using AccountService.Api.ViewModels;
 using AutoMapper;
 using MediatR;
 
 namespace AccountService.Api.Features.Transactions.ExecuteTransaction;
 
-public class ExecuteTransactionCommandHandler(IMediator mediator, IMapper mapper) : IRequestHandler<ExecuteTransactionCommand, TransactionViewModel>
+public class ExecuteTransactionCommandHandler(IUnitOfWork unitOfWork, IMediator mediator, IMapper mapper) : IRequestHandler<ExecuteTransactionCommand, TransactionViewModel>
 {
     public async Task<TransactionViewModel> Handle(ExecuteTransactionCommand request, CancellationToken cancellationToken)
     {
-        var transaction = await mediator.Send(mapper.Map<RegisterTransactionCommand>(request), cancellationToken);
-        await mediator.Send(new ApplyTransactionCommand { TransactionId = transaction.Id}, cancellationToken);
+        await unitOfWork.BeginTransactionAsync(cancellationToken);
 
-        return transaction;
+        try
+        {
+            var transaction = await mediator.Send(mapper.Map<RegisterTransactionCommand>(request), cancellationToken);
+            await mediator.Send(new ApplyTransactionCommand { TransactionId = transaction.Id }, cancellationToken);
+
+            await unitOfWork.CommitAsync(cancellationToken);
+
+            return transaction;
+        }
+        catch (Exception ex)
+        {
+            await unitOfWork.RollbackAsync(cancellationToken);
+            throw new UnprocessableException("Не удалось перенести деньги", ex);
+        }
     }
 }
--- src/AccountService.Api/ObjectStorage/UnitOfWork.cs
+++ src/AccountService.Api/ObjectStorage/UnitOfWork.cs
@@ -1,46 +1,48 @@
 ﻿using AccountService.Api.ObjectStorage.Interfaces;
+using AccountService.Infrastructure;
 using AccountService.Infrastructure.Models;
 using AccountService.Infrastructure.Repositories.Interfaces;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Storage;
 
 namespace AccountService.Api.ObjectStorage;
 
-public class UnitOfWork(DbContext context, IServiceProvider serviceProvider) : IUnitOfWork, IDisposable
+public class UnitOfWork(ApplicationDbContext context, IServiceProvider serviceProvider) : IUnitOfWork, IDisposable
 {
     private IDbContextTransaction? _transaction;
 
-    public async Task BeginTransactionAsync()
+    public async Task<IDbContextTransaction> BeginTransactionAsync(CancellationToken cancellationToken)
     {
         if (_transaction is { })
         {
             throw new InvalidOperationException("Transaction already started");
         }
 
-        _transaction = await context.Database.BeginTransactionAsync();
+        _transaction = await context.Database.BeginTransactionAsync(cancellationToken);
+
+        return _transaction;
     }
 
-    public async Task CommitAsync()
+    public async Task CommitAsync(CancellationToken cancellationToken)
     {
         if (_transaction is null)
         {
             throw new InvalidOperationException("No active transaction");
         }
 
-        await context.SaveChangesAsync();
-        await _transaction.CommitAsync();
+        await context.SaveChangesAsync(cancellationToken);
+        await _transaction.CommitAsync(cancellationToken);
         await _transaction.DisposeAsync();
         _transaction = null;
     }
 
-    public async Task RollbackAsync()
+    public async Task RollbackAsync(CancellationToken cancellationToken)
     {
         if (_transaction is null)
         {
             throw new InvalidOperationException("No active transaction");
         }
 
-        await _transaction.RollbackAsync();
+        await _transaction.RollbackAsync(cancellationToken);
         await _transaction.DisposeAsync();
         _transaction = null;
     }
@@ -56,8 +58,8 @@ public class UnitOfWork(DbContext context, IServiceProvider serviceProvider) : I
         return serviceProvider.GetRequiredService<T>();
     }
 
-    public async Task SaveChangesAsync() =>
-        await context.SaveChangesAsync();
+    public async Task SaveChangesAsync(CancellationToken cancellationToken) =>
+        await context.SaveChangesAsync(cancellationToken);
 
     public IExecutionStrategy CreateExecutionStrategy() =>
         context.Database.CreateExecutionStrategy();
